f61fd325e290d9a342ced514cf48a59f8272a35c
Carlos Ernesto <salgadocarlosernesto11@gmail.com>
2021-10-16 22:08:41 -0400

    Insula Bot Done

---
+++ bot.py
@@ -0,0 +1,188 @@
+import logging
+import os
+
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler, Filters
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.conversationhandler import ConversationHandler
+from telegram.ext.updater import Updater
+from telegram.inline.inlinekeyboardbutton import InlineKeyboardButton
+from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
+
+from sender import collection_1, send_collection_all
+
+
+# Configuracion del loogin
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+# Estados de la conversacion
+GETOPTION, GETCOMMENTS, GETSUBMENU = range(3)
+
+# Teclado Principal
+reply_keyboard = [
+    ['💎Nuestras Colecciones💎'],
+    ['Redes 💻📱📸', 'Quiénes somos❓'],
+    ['Por qué #pendientes_a_lo_cubano❓🤔'],
+    ['Comentar ✍️✍️'],
+    ['Quiero un BOT así😍']
+]
+markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+
+# Respuestas a los botones
+btn_1 = '^💎Nuestras Colecciones💎$'
+btn_2 = '^Redes 💻📱📸$'
+btn_3 = '^Quiénes somos❓$'
+btn_4 = '^Por qué #pendientes_a_lo_cubano❓🤔$'
+btn_5 = '^Comentar ✍️✍️$'
+btn_6 = '^Quiero un BOT así😍$'
+
+# Teclado de Colecciones
+colection_keyboard = [
+    ['Raíces Hispanas💃🏻♥️💛'],
+    ['Raíces Africanas👸🥁🌈'],
+    ['Paisajes Insulares🌊🏔🏛🏕'],
+    ['Ajiaco🌽🐷🥘🥣'],
+    ['Volver al menú↩️']
+]
+markup_colection = ReplyKeyboardMarkup(colection_keyboard, resize_keyboard=True)
+
+# Funciones para los manejadores
+def start(update: Update, context: CallbackContext):
+    usuario = update.effective_user.full_name
+    logger.info(f'Usuario: {usuario} ha iniciado en el bot.')
+    
+    update.message.reply_markdown_v2(
+        text='😉 ÍNSULA PENDIENTES A LO CUBANO 😉',
+        reply_markup=markup
+    )
+    return GETOPTION
+
+def colection(update: Update, context: CallbackContext):
+    usuario = update.effective_user.full_name
+    logger.info(f'Usuario: {usuario} esta en el Submenu de Coleccioes.')
+    
+    update.message.reply_text(
+        text='💎Nuestras Colecciones💎',
+        reply_markup=markup_colection
+    )
+    return GETSUBMENU
+
+def redes(update: Update, context: CallbackContext):
+    usuario = update.effective_user.full_name
+    logger.info(f'Usuario: {usuario} esta viendo info de redes Insula.')
+    
+    update.message.reply_text(
+        text="""La mejor modelo de 💎Ínsula eres tú.
+            Escoge👇🏻, encarga🛍 y luce🤩 tus #pendientes_a_lo_cubano
+            https://wa.me/c/5358037785
+            Síguenos y apoya nuestro proyecto por acá 👇🏻
+            https://www.instagram.com/insula_pendientes_a_lo_cubano/
+
+            Estamos en telegram😉👇🏻
+            @pendientesAloCubano
+            https://t.me/pendientesAloCubano
+
+            WhatsApp.com (https://wa.me/c/5358037785)
+            View Elizabeth's Catalog on WhatsApp
+            Learn more about their products & services"""
+    )
+    return GETOPTION
+
+
+def who_are(update: Update, context: CallbackContext):
+    usuario = update.effective_user.full_name
+    logger.info(f'Usuario: {usuario} quiere conocer mas de Isula.')
+    
+    update.message.chat.send_photo(
+        photo=open('./img/logo.jpg', 'rb'), 
+        caption="Ínsula es sinónimo de isla🏝. La intención esencial es defender un concepto basado en la cubanía❤️💙🤍. Por Ínsula nos referimos a la Isla de Cuba🇨🇺. Nuestros diseños resultan elementos esenciales de nuestra identidad cubana💎."
+    )
+    return GETOPTION
+
+def what_pc(update: Update, context: CallbackContext):
+    usuario = update.effective_user.full_name
+    logger.info(f'Usuario: {usuario} quiere conocer Pendientes a lo cubano.')
+    
+    update.message.reply_text(
+        text='Por qué #pendientes_a_lo_cubano?🤔\nEn nuestro eslogan hay un pequeño juego de doble sentido💡: \n✅En primer lugar nos referimos a los aretes con un sello nacional💎: "pendientes a lo cubano".\n✅Al mismo tiempo estamos "atentos" (pendientes) a lo singular, a lo típico, a lo cubano🇨🇺.'
+    )
+    return GETOPTION
+
+def to_comment(update: Update, context: CallbackContext):
+    usuario = update.effective_user.full_name
+    logger.info(f'Usuario: {usuario} comenta.')
+    
+    update.message.reply_text(
+        text='Por favor, deje su comentario sobre nuestro emprendimiento. Es muy importante para nosotros saber que piensas. 😉😉😉👇🏻👇🏻👇🏻'
+    )
+    return GETCOMMENTS 
+
+def save_comment(update: Update, context: CallbackContext):
+    user_name = update.effective_user.full_name
+    date = update.message.date.date()
+    comment = update.message.text
+    context.bot.sendMessage(
+        chat_id=1020450443,
+        text=f'Comentario: \nUsuario: {user_name}\nFecha: {date} \nComentario: \n{comment}' 
+    )
+    return GETOPTION
+
+def send_collection(update: Update, context: CallbackContext):
+    name_coll = update.message.text # -> Tomo el nombre de la colleccion q elije el usuario
+    print(name_coll)
+    send_collection_all(update, context, name_coll) 
+    return GETSUBMENU
+
+def spam(update: Update, context: CallbackContext):
+    usuario = update.effective_user.full_name
+    logger.info(f'Usuario: {usuario} quiere contratar tu servicio.')
+    
+    update.message.reply_text(
+        text='Puede tener un BOT 🤖 como este para automatizar su negocio, haciéndolo mas próspero.\nPuede perzonalizarlo a su gusto, atendiendo a sus necesidades, solo tiene que contactar con el desarrollador. 👇🏻👇🏻👇🏻 ',
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text='Contactatenos 🤓', url='https://t.me/CarlosSalgado')]])
+    )
+    return GETOPTION
+def main():
+    # TOKEN
+    TOKEN = os.getenv('TOKEN')
+    
+    # Updater
+    updater = Updater(TOKEN)
+    
+    # Dispatcher
+    dispatcher = updater.dispatcher
+    
+    conv_handler = ConversationHandler(
+        entry_points= [CommandHandler('start', start)],
+        states={
+            GETOPTION: [
+                MessageHandler(Filters.regex(btn_1), colection),
+                MessageHandler(Filters.regex(btn_2), redes),
+                MessageHandler(Filters.regex(btn_3), who_are),
+                MessageHandler(Filters.regex(btn_4), what_pc),
+                MessageHandler(Filters.regex(btn_5), to_comment),
+                MessageHandler(Filters.regex(btn_6), spam)
+            ],
+            GETSUBMENU:[ 
+                MessageHandler(Filters.regex('^Volver al menú↩️$'), start),
+                MessageHandler(Filters.regex('^Raíces Hispanas💃🏻♥️💛$'), send_collection),
+                MessageHandler(Filters.regex('^Raíces Africanas👸🥁🌈$'), send_collection),
+                MessageHandler(Filters.regex('^Paisajes Insulares🌊🏔🏛🏕$'), send_collection),
+                MessageHandler(Filters.regex('^Ajiaco🌽🐷🥘🥣$'), send_collection),
+                ],
+            GETCOMMENTS: [ MessageHandler(Filters.text, save_comment)]
+        },
+        fallbacks=[]
+    )
+    # Agregar la conversacion
+    dispatcher.add_handler(conv_handler)
+    # START BOT
+    updater.start_polling()
+    
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
---
+++ sender.py
@@ -0,0 +1,48 @@
+from telegram import Update, InlineKeyboardButton, keyboardbutton
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.inline.inlinekeyboardmarkup import InlineKeyboardMarkup
+
+# Infografias de cada coleccion
+collection_1 = ['La Colección Raíces Hispanas representa la herencia de nuestro "abuelo blanco"💛❤️🇪🇦Olé👏🏻! El poeta nacional Nicolás Guillén✒️🇨🇺 se refirió así a nuestros ancestros españoles en su Balada de los Dos Abuelos', 'Conoces nuestras Castañuelas??🤔🤔 Las protagonistas de los tablaos👏🏻👏🏻. Compañía por excelencia de cantaores🎤 y bailaores💃 Luce Hispanidad🇪🇸 Luces Castañuelas💛❤️', 'Conoces nuestros Abanicos🤔🤔 Códigos de romance😍 y coqueteo😏 Traen brisas de galanteo😉 y baile💃 Luce enigmática y sedutora💫 Luce Abanicos💛❤️','Conoces nuestros Colores Hispanos??🤔🤔 Binomio identitario de los peninsulares: Amarillo💛 y Rojo❤️ Marca la herencia de nuestro "abuelo blanco"👴🏻(al decir de Guillén✒️) Luce Colores Hispanos🇪🇦Olé', 'Conoces nuestras Batas Sevillanas??🤔🤔 Bailan al ritmo de las palmas 💃👏🏻👏🏻 Llenan de lunares y colores las ferias sevillanas 🎉🎉 Luce la elegancia andaluza😌 Luce Batas Sevillanas💛❤️','Conoces nuestras Batas Sevillanas??🤔🤔 Bailan al ritmo de las palmas 💃👏🏻👏🏻 Llenan de lunares y colores las ferias sevillanas 🎉🎉 Luce la elegancia andaluza😌 Luce Batas Sevillanas💛❤️']
+collection_2 = ['La Colección Raíces Africanas representa la herencia de "nuestro abuelo negro". Ashé 😉','Ínsula 🏝: Conoces nuestro Mestizaje??🤔🤔 ¿Qué somos los cubanos sino una mezcla única y hermosa de muchas raíces?🌈  Somos una Isla mestiza, múltiple, diversa🏝🎨♥️ Luce Cubanía🇨🇺 Luce Mestizaje', 'Conoces nuestros Tambores??🤔🤔 A su toque baila nuestra identidad 🥁💫 Se acompañan de risas😄, de palmas👏🏻, de canto🎤🎶 La sensualidad cubana se resume en su sonido😏😉🥁🎶 Luce nuestro Folclore ♥️💙 Luce Tambores', 'Conoces nuestros Oshún??🤔🤔 La reina de la feminidad👸 y el romance♥️ Patrona nuestra💫🙇🏼♀️ Con esencia de miel🍯 y girasoles🌻 Luce sensualidad ancestral 💛💄👑 Luce Oshún', 'Conoces nuestros Yemayá??🤔🤔 La reina de las aguas 👸💙y los mares🌊🐚 Símbolo de maternidad🤰 y amor💖 Luce serenidad☺️ y paz🧘♀️ Luce Yemayá']
+collection_3 = ['La Colección Paisajes Insulares recrea lugares representativos de nuestra geografía. Viaja con ella 😉','Ínsula 🏝: Conoces nuestro Viñales??🤔🤔 Paraíso natural único 💚✨ Combinación perfecta de ríos🌊 , valles 🏕, mogotes ⛰y cuevas🏔 Luce Exótica 😉🤩 Luce Viñales','Conoces nuestro Varadero??🤔🤔 La mejor playa del Caribe 🏖🥇 Baño de aguas azules 🌊 espuma blanca y arena fina💎 Luce Tropical👒⛱   Luce Varadero','Conoces nuestra Habana??🤔🤔 La ciudad de las columnas🏛  Novia de la bahía ❤🌊 Mezcla de tradición🧓 y modernidad 😎 Luce Bohemia🥰 Luce La Habana', 'Conoces nuestro Turquino??🤔🤔 El punto más elevado de nuestra geografía⛰ Ícono de cubanía🇨🇺😌 Luce aventura 🚵🏼♀️ e Identidad❤💙 Luce Turquino⛰💫']
+collection_4 = ['Ajiaco Ínsula 🏝: La colección Ajiaco es la expresión de la cubanía hecha al plato','Conoces nuestro Cerdo Asado??🤔🤔 El rey de nuestras mesas🥩🐷💙♥️ Protagonista del fin de año🎊🗓⏳ Luce singular💫🤩 Luce Cerdo Asado', 'Conoces nuestra Caldosa??🤔🤔 Sabor y aroma de las fiestas cubanas😋🥘♥️💙 Mezcla perfecta de ingredientes y condimentos 🌽🌶🥔🍗🍖🥩🥓 El más cubano de los caldos 😌🥘 Luce festiva🎊 Luce Caldosa', 'Conoces nuestros Tamalitos de Olga??🤔🤔 Envoltorio de tradiciones 🌽💛💚 Pican, no pican, los tamalitos de Olga🎶 Luce colorido🌈 Luces Los tamalitos de Olga', 'Conoces nuestros Moros y Cristianos??🤔🤔 La expresión de la cubanía hecha plato🇨🇺🥣 Mestizaje de sabores 🌈😋en singular unión👩🏻🍳 Luce la elegancia típica 😌♥️💙 Luce Moros y Cristianos']
+c_patt = ['rh', 'ra', 'pi', 'a']
+
+# Botonera para compra por whatsapp y telegram
+keyboard = [
+    [InlineKeyboardButton(text='🛒Comprar🛒', callback_data='#')],
+    [InlineKeyboardButton(text='🛒 Por WhatsApp 🛒', url='https://wa.me/5358037785'), InlineKeyboardButton(text='🛒 Por Telegram 🛒', url='https://t.me/Elizabeth_Salgado')]
+]
+markup = InlineKeyboardMarkup(keyboard)
+
+def send_collection_all(update: Update, context: CallbackContext, name_coll):
+    if name_coll == 'Raíces Hispanas💃🏻♥️💛': 
+        for i in range(6):
+            update.message.chat.send_photo( 
+                    photo=open(f'./img/col_{c_patt[0]}_{i}.jpg', 'rb'),
+                    caption=collection_1[i],
+                    reply_markup=markup
+            )
+    elif name_coll == 'Raíces Africanas👸🥁🌈':
+        for i in range(5):
+            update.message.chat.send_photo( 
+                    photo=open(f'./img/col_{c_patt[1]}_{i}.jpg', 'rb'),
+                    caption=collection_2[i],
+                    reply_markup=markup
+            )        
+    elif name_coll == 'Paisajes Insulares🌊🏔🏛🏕':
+        for i in range(5):
+            update.message.chat.send_photo( 
+                    photo=open(f'./img/col_{c_patt[2]}_{i}.jpg', 'rb'),
+                    caption=collection_3[i],
+                    reply_markup=markup
+            )        
+    elif name_coll == 'Ajiaco🌽🐷🥘🥣':
+        for i in range(5):
+            update.message.chat.send_photo( 
+                    photo=open(f'./img/col_{c_patt[3]}_{i}.jpg', 'rb'),
+                    caption=collection_4[i],
+                    reply_markup=markup
+            )        
+    
